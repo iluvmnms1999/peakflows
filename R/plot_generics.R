6813532ca798a30d75b437c8522512538885fbaa
ccmantzc <clarissa@mantz.us>
2023-04-13 11:43:53 -0600

    editing plot_generic

--- R/plot_generics.R
+++ R/plot_generics.R
@@ -1,6 +1,6 @@
 # # graph for verification
-# sub <- filter(shv_tot, id == "10311100")
-# peaks <- filter(peaks_df, id == "10311100")
+# # sub <- dplyr::filter(rhv_tot, id == "10311100")
+# # peaks <- dplyr::filter(peaks_df, id == "10311100")
 #
 # # ros classification plot
 # ggplot(sub, aes(x = as.POSIXct(datetime), y = max_flow)) +
@@ -8,8 +8,8 @@
 #   ggtitle("2017: Streamflow Peaks at Station 10311100") +
 #   theme(plot.title = element_text(hjust = 0.5)) +
 #   xlab("DateTime") +
-#   xlim(ymd_h(2017020101, tz = "US/Pacific"),
-#        ymd_h(2017022823, tz = "US/Pacific")) +
+#   xlim(lubridate::ymd_h(2017020101, tz = "US/Pacific"),
+#        lubridate::ymd_h(2017022823, tz = "US/Pacific")) +
 #   ylab("Discharge amount (ft^3 per sec)") +
 #   scale_y_continuous(limits = c(0, 140), breaks = seq(0, 140, 20)) +
 #   # add points for defined peaks
@@ -20,3 +20,27 @@
 #                                    "#41ab5d", "#bf812d")
 #              )
 #   )
+#
+# Creating the function...
+# plot.peakflows <- function(station, peaks, x, y, ...) {
+#   sub <- dplyr::filter(rhv_tot, id == as.character(station))
+#   ggplot2::ggplot(sub, aes(x = as.POSIXct(datetime), y = max_flow)) +
+#     geom_point(size = .2, col = "grey50") +
+#     ggtitle("2017: Streamflow Peaks at Station 10311100") +
+#     theme(plot.title = element_text(hjust = 0.5)) +
+#     xlab("DateTime") +
+#     xlim(lubridate::ymd_h(2017020101, tz = "US/Pacific"),
+#          lubridate::ymd_h(2017022823, tz = "US/Pacific")) +
+#     ylab("Discharge amount (ft^3 per sec)") +
+#     scale_y_continuous(limits = c(0, 140), breaks = seq(0, 140, 20)) +
+#     # add points for defined peaks
+#     peaks_df <- df_peaks_filt(station, peaks)
+#     geom_point(data = peaks_df, aes(as.POSIXct(dt), y),
+#                color = ifelse(peaks_df$type %in% c("major", "flood"),
+#                               "red3",
+#                               ifelse(peaks_df$type == "minor",
+#                                      "gold", "green3")
+#                )
+#     )
+# }
+
